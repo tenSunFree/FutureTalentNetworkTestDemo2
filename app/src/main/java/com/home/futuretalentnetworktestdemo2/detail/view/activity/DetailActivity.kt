38f510d56288272862f67aa8d34b3054613e4acf
Sun <sunwenyen@gmail.com>
2020-05-31 03:56:25 +0800

    add detail ui layout

---
+++ app/src/main/java/com/home/futuretalentnetworktestdemo2/detail/view/activity/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.home.futuretalentnetworktestdemo2.detail.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.home.futuretalentnetworktestdemo2.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
