fe2a438f747c336378a8d62ab4d383860ec03907
Sun <sunwenyen@gmail.com>
2020-05-31 04:06:33 +0800

    add pagination ui layout

---
+++ app/src/main/java/com/home/futuretalentnetworktestdemo2/pagination/PaginationActivity.kt
@@ -0,0 +1,13 @@
+package com.home.futuretalentnetworktestdemo2.pagination
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.home.futuretalentnetworktestdemo2.R
+
+class PaginationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagination)
+    }
+}
